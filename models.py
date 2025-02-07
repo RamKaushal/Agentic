9f9b7d01207db39d68c3944bbe40db595c03f823
RamKaushal <ramkaushalguralla@gmail.com>
2025-02-07 17:54:23 +0530

    fixed init in main

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 class Forecasting_models:
-    def __init__(self,train_df,test_df,forecsat_days):
+    def __init__(self,train_df,test_df,forecast_days):
         """
         Class init to input train,test and nuber of days to forecast
         """
-        self.df = df
+        self.train_df = train_df
+        self.test_df = test_df
+        self.forecast_days = forecast_days
         return None
-    
-    def XGBOOST(self):
-        return forecast_df
+  
