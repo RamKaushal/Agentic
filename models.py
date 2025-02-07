9c384bcfff759aecc2de7b7fb4adb456bf87a086
Ram kaushal Gurrala <55400660+RamKaushal@users.noreply.github.com>
2025-02-07 13:26:35 +0530

    created a class for forecasting and added XG boost skeleton

---
+++ models.py
@@ -0,0 +1,10 @@
+class Forecasting_models:
+    def __init__(self,train_df,test_df,forecsat_days):
+        """
+        Class init to input train,test and nuber of days to forecast
+        """
+        self.df = df
+        return None
+    
+    def XGBOOST(self):
+        return forecast_df
