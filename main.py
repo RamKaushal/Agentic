fea9ca2f6228efc3934f36024a10835110eef4ca
RamKaushal <ramkaushalguralla@gmail.com>
2025-02-07 17:31:54 +0530

    added config yaml to make param changes and read forecast days from there

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas as pd
-
 from utils import write_data_db,read_data_db
+import yaml 
 
-df = pd.read_csv(r"C:\Users\ramka\Downloads\Agentic-main\Agentic\ACD call volume.csv")
-write_data_db(df,"ACD_VOLUME")
+
+# df = pd.read_csv(r"C:\Users\ramka\Downloads\Agentic-main\Agentic\ACD call volume.csv")
+# write_data_db(df,"ACD_VOLUME")
 
 query = "SELECT * FROM ACD_VOLUME"
 read_df = read_data_db(query)
 
-print(read_df.head())
+with open(r"C:\Users\ramka\Downloads\Agentic-main\Agentic\config.yaml","r") as f:
+    config = yaml.safe_load(f)
+    
+
+forecast_days = config['forecast_days']
+print(forecast_days)
+
