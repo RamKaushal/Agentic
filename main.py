ea796ddd7026553e643012c1d0c49a0103b069cc
RamKaushal <ramkaushalguralla@gmail.com>
2025-02-07 17:44:16 +0530

    added logger function and modified main to log the events

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pandas as pd
-from utils import write_data_db,read_data_db
+from utils import write_data_db,read_data_db,get_logger
 import yaml 
 
+logger = get_logger()
 
-# df = pd.read_csv(r"C:\Users\ramka\Downloads\Agentic-main\Agentic\ACD call volume.csv")
-# write_data_db(df,"ACD_VOLUME")
+#Write data into server
+try:
+    df = pd.read_csv(r"C:\Users\ramka\Downloads\Agentic-main\Agentic\ACD call volume.csv")
+    write_data_db(df,"ACD_VOLUME")
+    logger.info(f"Data is pushed into DB")
+except Exception as e:
+    logger.error(f"Failed to pushed data into server becasue of {e}")
 
-query = "SELECT * FROM ACD_VOLUME"
-read_df = read_data_db(query)
+
+#read data into server
+try:
+    query = "SELECT * FROM ACD_VOLUME"
+    read_df = read_data_db(query)
+    logger.info(f"Data is read into DF FROM  DB")
+except Exception as e:
+    logger.error(f"Failed to read data from DB becasue of {e}")
 
 with open(r"C:\Users\ramka\Downloads\Agentic-main\Agentic\config.yaml","r") as f:
     config = yaml.safe_load(f)
     
 
 forecast_days = config['forecast_days']
+logger.info(f"Forecast days are read and set to {forecast_days}")
 print(forecast_days)
-
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+import logging
 
 def write_data_db(df,table_name):
     conn = sqlite3.connect("calls.db")
@@ -11,5 +12,25 @@ def read_data_db(query):
     conn = sqlite3.connect("calls.db")
     df_from_db = pd.read_sql(query, conn)
     return df_from_db
+
+import logging
+
+def get_logger():
+    logger = logging.getLogger("AGENTIC_PROJECT")
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+        file_handler = logging.FileHandler("logfile.txt", mode="a")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+
 
 
