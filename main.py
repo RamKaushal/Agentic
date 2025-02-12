cd2ef7075ba5b43c9843382fb995500a4827e669
RamKaushal <ramkaushalguralla@gmail.com>
2025-02-12 19:20:04 +0530

    forgot the timestamp so the recent values were not pulling up , Fixed the bug

--- main.py
+++ main.py
@@ -113,18 +113,21 @@ def scenario2(forecast_days):
         df_retrain['Date'] = pd.to_datetime(df_retrain['Date'], format="%d-%m-%Y") #converting to datetime
         forecast_obj = ForecastingModels(df_retrain, forecast_days) #creating an object from the forecasting models class, passing the df_read which have data till nov3
         trained_model = forecast_obj.train_xgb_model() #This will train our XGB model till NOv 3
-        
         forecast_df = forecast_obj.forecast_xgb_model(trained_model) #passing the model to the class
         forecast_df['Date'] = pd.to_datetime(forecast_df['Date'], format="%d-%m-%Y") #converting to datetime
-        
         df_retrain['Date'] = pd.to_datetime(df_retrain['Date'], format="%d-%m-%Y") #converting to datetime
         forecast_df = forecast_df.iloc[lag_days:]
         max_date = forecast_df['Date'].max() #getting max of date for logs
         min_date = forecast_df['Date'].min() #getting min of date for logs
         min_date_r = df_retrain['Date'].min()
         max_date_r = df_retrain['Date'].max()
-        write_data_db(df_retrain, "ACD_VOLUME_TRAIN","append") #wring the train data back to DB
-        write_data_db(forecast_df, "ACD_VOLUME_FORECAST","append") #writng the forecast data back to DB
+        df_retrain['Timestamp'] = datetime.now() 
+        forecast_df['Timestamp'] = datetime.now() 
+        try:
+            write_data_db(df_retrain, "ACD_VOLUME_TRAIN","append") #wring the train data back to DB
+            write_data_db(forecast_df, "ACD_VOLUME_FORECAST","append") #writng the forecast data back to DB
+        except:
+            print("error")
         plot_line_chart(df_retrain,x='Date',y='Call Volume',label1="Call Volume Train",df1 = forecast_df,x1='Date',x2='Predicted_Call_Volume',label2="Call Volume Forecasted") #linechart of train and predicted
         logger.info(f"Forecasting for next {forecast_days-lag_days} days completed from {min_date} to {max_date}")
         logger.info(f"FORECAST Data is pushed into DB and forecast is {forecast_df}")
@@ -146,7 +149,7 @@ def retrain_actuals(forecast_days):
         #save the retrained model
     try:
         query = f"""
-            SELECT * FROM ACD_VOLUME_TRAIN 
+            SELECT * FROM ACD_VOLUME_TRAIN
             WHERE Timestamp = (SELECT MAX(Timestamp) FROM ACD_VOLUME_TRAIN)
         """
         df_train = read_data_db(query) #query to pull the training data, to append the next 7 days of actual data
@@ -247,6 +250,6 @@ if __name__ == "__main__":
     logger.info(f"Forecast days are read and set to {forecast_days}")
     logger.info(f"Training data till {train_date}")
     # total_data_push(train_date,forecast_days,lag_days) #this function needs to run one time (create XGB model and trains it and generates forecast for 14 days)
-    scenario2(forecast_days)
-    # retrain_actuals(forecast_days) #This function needs to run in loop to simulates sub sequent weeks
+    # scenario2(forecast_days)
+    retrain_actuals(forecast_days) #This function needs to run in loop to simulates sub sequent weeks
     
