3aec9123dfaca3144907a2366b67c006977ff5a2
RamKaushal <ramkaushalguralla@gmail.com>
2025-02-12 18:58:40 +0530

    fixed the irritating bug of index

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def total_data_push(train_date,forecast_days,lag_days):
      
 #SCenario 2: 1 week later re-train on last 7 days
 def scenario2(forecast_days):
-    # try:   
+    try:   
         logger.info(f"SCENARIO 2 of retraining last 7 days started")
         query = f"""
             SELECT * FROM ACD_VOLUME_TRAIN 
@@ -91,8 +91,6 @@ def scenario2(forecast_days):
         df_train['Date'] = pd.to_datetime(df_train['Date']) #converting to datetime
         df_train = df_train[['Date', 'Call Volume', 'U.S. Holiday Indicator', 'Call Volume Impact', 'Day of Week', 'Day of Month', 'Day of Year', 'Week of Year', 'Month', 'Quarter', 'Is Weekend']] #getting all the columns other than timestamp column
         df_train['Date'] = pd.to_datetime(df_train['Date']) #converting to datetime
-        print(df_train.tail(10))
-        
         df_actual_retrain_query =  f"""
             WITH cte1 AS (
                 SELECT * 
@@ -106,14 +104,13 @@ def scenario2(forecast_days):
             SELECT * FROM cte1 order by Date Desc limit 7;
         """
         df_actual_retrain = read_data_db(df_actual_retrain_query) 
-
         df_actual_retrain['Date'] = pd.to_datetime(df_actual_retrain['Date'])
-        df_actual_retrain = df_actual_retrain.sort_values(by=['Date']).reset_index()
+        df_actual_retrain = df_actual_retrain.sort_values(by=['Date']).reset_index(drop=True)
         max_date = df_actual_retrain['Date'].max() #getting max of date for logs
         min_date = df_actual_retrain['Date'].min() #getting min of date for logs
         logger.info(f"values of  {min_date} to {max_date} are added and are being retrained")
         df_retrain = pd.concat([df_train, df_actual_retrain], ignore_index=True)
-
+        df_retrain['Date'] = pd.to_datetime(df_retrain['Date'], format="%d-%m-%Y") #converting to datetime
         forecast_obj = ForecastingModels(df_retrain, forecast_days) #creating an object from the forecasting models class, passing the df_read which have data till nov3
         trained_model = forecast_obj.train_xgb_model() #This will train our XGB model till NOv 3
         
@@ -134,8 +131,8 @@ def scenario2(forecast_days):
         logger.info(f"TRAIN Data is pushed into DB from {min_date_r} to {max_date_r}")
         joblib.dump(trained_model, "xgb_model.pkl") #Dumping our model so that we can pull on monday and forecast it
         logger.info(f"XGB model trained and saved successfully")
-    # except Exception as e:
-    #     logger.error(f"Prediction failed: {e}")
+    except Exception as e:
+        logger.error(f"Prediction failed: {e}")
         return None
 
 
